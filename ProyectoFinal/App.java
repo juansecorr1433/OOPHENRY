b1ebcd257eea5effe58f11d2b90f28e342ee372b
juansecorr1433 <jscorreaw@udistrital.edu.co>
2025-12-11 16:23:56 -0500

    Add files via upload

---
+++ ProyectoFinal/App.java
@@ -0,0 +1,16 @@
+/*Proyecto Final - Programacion orientada a Objetos
+Luis Miguel Hidalgo Cansino - 20251005117
+Juan Sebastian Correa Walteros 20251005092
+Diego Alexander Huertas Sosa - 20251005115
+
+Sistema de Gestion de Restaurante
+*/
+
+
+import controlador.Controlador;
+
+public class App {
+    public static void main(String[] args) {
+        new Controlador();
+    }
+}
